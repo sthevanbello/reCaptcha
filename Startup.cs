c0b22c2c861d9b3980e5a2e9417f1fc433e03535
sthevanbello <sthevan.alves@gmail.com>
2022-12-26 12:39:39 -0300

    Revert "Efetuando testes no Sourcetree modificando a classe Startup"
    This reverts commit d5f345d99fb85fd133c08a855ea792c0c26186aa.

--- Startup.cs
+++ Startup.cs
@@ -20,14 +20,15 @@ namespace reCaptcha
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            // Startup modificada pela primeira vez
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -35,6 +36,7 @@ namespace reCaptcha
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
