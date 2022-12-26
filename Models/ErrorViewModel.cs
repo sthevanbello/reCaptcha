e06adbea3c6dfdb56f7ffc264aeec0563f1d5596
sthevanbello <sthevan.alves@gmail.com>
2022-12-26 12:30:04 -0300

    Testes com reCaptcha V3 em um projeto web mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reCaptcha.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
