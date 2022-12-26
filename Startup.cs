d5f345d99fb85fd133c08a855ea792c0c26186aa
sthevanbello <sthevan.alves@gmail.com>
2022-12-26 12:35:32 -0300

    Efetuando testes no Sourcetree modificando a classe Startup

--- Startup.cs
+++ Startup.cs
@@ -20,15 +20,14 @@ namespace reCaptcha
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Startup modificada pela primeira vez
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,7 +35,6 @@ namespace reCaptcha
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
